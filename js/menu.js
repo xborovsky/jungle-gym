b26ffe96d26d300cdaf3ca9ffc1f323d18bbbffe
xborovsky <xborovsky@gmail.com>
2020-08-20 10:49:35 +0200

    Top menu

--- js/menu.js
+++ js/menu.js
@@ -1,18 +1,27 @@
 var menuDisplayHandler = (function() {
-    var $navbarContainer = $('#navbar').parent(),
-        $navbarBrand = $navbarContainer.find('.navbar-brand');
+    var $navbarContainer = $('#navbar').parent();
 
     var init = function() {
-        $navbarBrand.attr('style', 'display: none !important');
-
         if ($(window).width() < 768) {
             showMobileMenu();
+
+            $('.navbar-toggler, .nav-link').click(function() {
+                var $toggler = $('.navbar-toggler');
+                setTimeout(function() {
+                    if ($toggler.hasClass('collapsed')) {
+                        $('#navbar-container').addClass('transparent');
+                        $('.navbar-brand').addClass('hidden');
+                    } else {
+                        $('#navbar-container').removeClass('transparent');
+                        $('.navbar-brand').removeClass('hidden');
+                    }
+                }, 5);
+            });
         }
     };
 
     var showMobileMenu = function() {
         $navbarContainer.addClass('navbar-fixed-top');
-        $navbarBrand.attr('style', 'display: block !important');
     };
 
     return {

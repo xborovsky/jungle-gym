8e1a4f282836e5972ea4912757f22ca36c409246
23boro23 <mborovsky@zoznam.sk>
2020-08-26 13:43:07 +0200

    Map popup with address

--- js/map.js
+++ js/map.js
@@ -3,5 +3,7 @@
     
     L.tileLayer.provider('Esri.WorldStreetMap').addTo(map);
 
-    L.marker([50.032244, 14.589419]).addTo(map);
+    L.marker([50.032244, 14.589419]).addTo(map)
+        .bindPopup('Františka Diviše 1282/56A<br />104 00, Praha 10')
+        .openPopup();
 })(jQuery);

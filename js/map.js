7b9d4bb2a2f0a0bde28d8611341ab83652ad411a
23boro23 <mborovsky@zoznam.sk>
2020-08-26 13:37:26 +0200

    Esri.WorldSteetMap layer provider

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 (function($) {
     var map = L.map('map').setView([50.032244, 14.589419], 16);
     
-    L.tileLayer.provider('Esri.WorldTopoMap').addTo(map);
+    L.tileLayer.provider('Esri.WorldSteetMap').addTo(map);
 
     L.marker([50.032244, 14.589419]).addTo(map);
 })(jQuery);
